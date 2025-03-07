e53a1f3b058fb3759c738675d31701e9629690fd
orbisonkovalik65 <orbisonkovalik65@gmail.com>
2025-03-07 19:20:25 +0000

    Add 2.c

---
+++ src/2.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main() {
+    int x = 5;
+    int y = 10;
+    if (x > y) {
+        printf("x is greater than y\n");
+    } else {
+        printf("x is less than or equal to y\n");
+    }
+    return 0;
+}
