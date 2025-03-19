76b98100092e239c69f4056b1059a192136ba323
orbisonkovalik65 <orbisonkovalik65@gmail.com>
2025-03-19 23:16:20 +0000

    Add 14.c

---
+++ src/14.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+
+int main() {
+    int x = 5;
+    if (x > 0) {
+        printf("X is positive\n");
+    } else {
+        printf("X is not positive\n");
+    }
+    return 0;
+}
