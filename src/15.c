bad0377218d62b32ad2a0ff35b3c17e31921b659
orbisonkovalik65 <orbisonkovalik65@gmail.com>
2025-03-20 08:57:12 +0000

    Add 15.c

---
+++ src/15.c
@@ -0,0 +1,14 @@
+
+int main() {
+    int x = 5;
+    float y = 3.14;
+    char z = 'a';
+    bool flag = true;
+    
+    if (flag) {
+        printf("The value of x is %d\n", x);
+        printf("The value of y is %f\n", y);
+        printf("The value of z is %c\n", z);
+    }
+    return 0;
+}
