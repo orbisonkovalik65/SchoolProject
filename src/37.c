aa05b6c40c4fb05313ebfd4a548a6a0eab5baacc
orbisonkovalik65 <orbisonkovalik65@gmail.com>
2025-05-17 04:09:28 +0000

    Add 37.c

---
+++ src/37.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+
+void main() {
+    int i;
+
+    printf("Hello, World!\n");
+    for (i = 0; i < 5; i++) {
+        if (i % 2 == 0) {
+            printf("%d ", i);
+        }
+    }
+
+    return 0;
+}
